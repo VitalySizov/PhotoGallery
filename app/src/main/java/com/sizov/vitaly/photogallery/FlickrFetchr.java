53ae13a5138e6c5241adae000a5f04de7f887aab
Vitaly Sizov <vitalii_sizov@mail.ru>
2017-09-28 23:00:17 +0600

    Chapter 25 completed

--- app/src/main/java/com/sizov/vitaly/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/sizov/vitaly/photogallery/FlickrFetchr.java
@@ -17,6 +17,16 @@ public class FlickrFetchr {
     private static final String TAG = "FlickrFetchr";
 
     private static final String API_KEY = "89f7a7e82da2b731da625dcf9b72b317";
+    private static final String FETCH_RECENT_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri
+            .parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -46,20 +56,23 @@ public class FlickrFetchr {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public List<GalleryItem> fetchItems(int page) {
+    // Получение последних фоток
+    public List<GalleryItem> fetchRecentPhotos() {
+        String url = buildUrl(FETCH_RECENT_METHOD, null);
+        return downloadGalleryItems(url);
+    }
+
+    // Поиск фото
+    public List<GalleryItem> searchPhotos(String query) {
+        String url = buildUrl(SEARCH_METHOD, query);
+        return downloadGalleryItems(url);
+    }
+
+    private List<GalleryItem> downloadGalleryItems(String url) {
 
         List<GalleryItem> items = new ArrayList<>();
 
         try {
-            String url = Uri.parse("https://api.flickr.com/services/rest/")
-                    .buildUpon()
-                    .appendQueryParameter("method", "flickr.photos.getRecent")
-                    .appendQueryParameter("api_key", API_KEY)
-                    .appendQueryParameter("format", "json")
-                    .appendQueryParameter("nojsoncallback", "1")
-                    .appendQueryParameter("extras", "url_s")
-                    .appendQueryParameter("page", Integer.toString(page))
-                    .build().toString();
             String jsonString = getUrlString(url);
             Log.d(TAG, "Received JSON: " + jsonString);
             parseItemsGson(items, jsonString);
@@ -69,6 +82,17 @@ public class FlickrFetchr {
         }
 
         return items;
+    }
+
+    // Вспомогательный метод для построения URL
+    private String buildUrl(String method, String query) {
+        Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method);
+        if (method.equals(SEARCH_METHOD)) {
+            uriBuilder.appendQueryParameter("text", query);
+        }
+
+        return uriBuilder.build().toString();
     }
 
     private void parseItemsGson (List<GalleryItem> items, String jsonString) {
--- app/src/main/java/com/sizov/vitaly/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/sizov/vitaly/photogallery/PhotoGalleryFragment.java
@@ -10,8 +10,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -40,7 +44,8 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        new FetchItemTask().execute();
+        setHasOptionsMenu(true);
+        updateItems();
 
         Handler responseHandler = new Handler();
         mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
@@ -76,7 +81,7 @@ public class PhotoGalleryFragment extends Fragment {
                 GridLayoutManager layoutManager = (GridLayoutManager)recyclerView.getLayoutManager();
                 int loadBufferPosition = 1;
                 if(lastPosition >= adapter.getItemCount() - layoutManager.getSpanCount() - loadBufferPosition) {
-                    new FetchItemTask().execute(lastPosition + 1);
+                    //new FetchItemTask().execute(lastPosition + 1);
                     //Toast.makeText(getActivity(), "Page Number " + lastFetchedPage, Toast.LENGTH_SHORT).show();
                 }
             }
@@ -97,6 +102,56 @@ public class PhotoGalleryFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         mThumbnailDownloader.clearQueue();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater);
+        menuInflater.inflate(R.menu.fragment_photo_gallery, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "QueryTextSubmit: " + query);
+                QueryPreferences.setStoredQuery(getActivity(), query);
+                updateItems();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "QueryTextChange: " + newText);
+                return false;
+            }
+        });
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = QueryPreferences.getStoredQuery(getActivity());
+                searchView.setQuery(query, false);
+            }
+        });
+    }
+
+    // Стирание сохраненного запроса
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_clear:
+                QueryPreferences.setStoredQuery(getActivity(), null);
+                updateItems();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateItems() {
+        String query = QueryPreferences.getStoredQuery(getActivity());
+        new FetchItemTask(query).execute();
     }
 
     @Override
@@ -161,38 +216,11 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(PhotoHolder photoholder, int position) {
-
+        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
             GalleryItem galleryItem = mGalleryItems.get(position);
-            Bitmap bitmap = mThumbnailDownloader.getCachedImage(galleryItem.getUrl());
-
-            if (bitmap == null) {
-                Drawable placeholder = getResources().getDrawable(R.drawable.bill_up_close);
-                photoholder.bindDrawable(placeholder);
-                mThumbnailDownloader.queueThumbnail(photoholder, galleryItem.getUrl());
-            } else {
-                Log.i(TAG, "Loaded image from cache");
-                photoholder.bindDrawable(new BitmapDrawable(getResources(), bitmap));
-            }
-
-            preLoadAdjacentImage(position);
-
-            //lastBoundPosition = position;
-            //Log.i(TAG,"Last bound position is " + Integer.toString(lastBoundPosition));
-        }
-
-        private void preLoadAdjacentImage(int position) {
-            final int imageBufferSize = 10;
-
-            int startIndex = Math.max(position - imageBufferSize, 0);
-            int endIndex = Math.min(position + imageBufferSize, mGalleryItems.size() - 1);
-
-            for (int i = startIndex; i <= endIndex; i++) {
-                if (i == position) continue;
-
-                String url = mGalleryItems.get(i).getUrl();
-                mThumbnailDownloader.preLoadImage(url);
-            }
+            Drawable placeholder = getResources().getDrawable(R.drawable.bill_up_close);
+            photoHolder.bindDrawable(placeholder);
+            mThumbnailDownloader.queueThumbnail(photoHolder, galleryItem.getUrl());
         }
 
         @Override
@@ -201,22 +229,27 @@ public class PhotoGalleryFragment extends Fragment {
         }
     }
 
-    private class FetchItemTask extends AsyncTask<Integer, Void, List<GalleryItem>>{
+    private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>>{
+        private String mQuery;
+
+        public FetchItemTask(String query) {
+            mQuery = query;
+        }
+
         @Override
-        protected List<GalleryItem> doInBackground(Integer... params) {
-            return new FlickrFetchr().fetchItems(lastFetchedPage);
+        protected List<GalleryItem> doInBackground(Void... params) {
+
+            if (mQuery == null) {
+                return new FlickrFetchr().fetchRecentPhotos();
+            } else {
+                return new FlickrFetchr().searchPhotos(mQuery);
+            }
         }
 
         @Override
         protected void onPostExecute(List<GalleryItem> items) {
-            if (lastFetchedPage > 1) {
-                mItems.addAll(items);
-                mPhotoRecyclerView.getAdapter().notifyDataSetChanged();
-            } else {
-                mItems = items;
-                setupAdapter();
-            }
-            lastFetchedPage++;
+            mItems = items;
+            setupAdapter();
         }
     }
 }
