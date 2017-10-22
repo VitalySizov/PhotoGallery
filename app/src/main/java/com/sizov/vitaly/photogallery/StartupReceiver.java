cb073d360758beaf7a1cc34692eb23d2fd528d3c
Vitaly Sizov <vitalii_sizov@mail.ru>
2017-10-22 16:58:04 +0600

    Chapter 27 completed

---
+++ app/src/main/java/com/sizov/vitaly/photogallery/StartupReceiver.java
@@ -0,0 +1,20 @@
+package com.sizov.vitaly.photogallery;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        // Включение сигнала при загрузке
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
+    }
+}
